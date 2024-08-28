28680a9ba3beea8b289d0c59fc42b50710575084
Halil Ömer <halilomergurgan@gmail.com>
2024-08-29 02:00:06 +0300

    fixed oauth connect

---
+++ db/migrate/20240828215133_create_todos.rb
@@ -0,0 +1,14 @@
+class CreateTodos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.text :description
+      t.datetime :due_date
+      t.integer :priority
+      t.boolean :completed
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
