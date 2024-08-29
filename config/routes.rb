f2e7d97e5cdc0429f73c199dd69506c92708ca3f
Halil Ömer <halilomergurgan@gmail.com>
2024-08-29 11:49:01 +0300

    setup login and register page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  get 'todos/new'
-  get 'todos/create'
   root 'pages#home'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :todos
 end
